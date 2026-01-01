14909a61889b367bc7fa1f34120722514f00ebd8
Shailesh Mandani <shaileshmandani5304@gmail.com>
2026-01-01 17:35:21 +0000

    conversions  Operations

---
+++ 01_Basics/03_conversionsOperation.js
@@ -0,0 +1,31 @@
+// let score = null;
+// console.log(typeof score);
+
+// let valueINnumber = Number(score)
+// console.log(typeof valueINnumber);
+// console.log(valueINnumber);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//**************** OPERATIONS ********************************* */
+
+
+let value = 3;
+
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello"; 
+let str2 = " shailesh"; 
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(3 + "3");
+// console.log("3" + 3);
+// console.log("3" + "3");
+
