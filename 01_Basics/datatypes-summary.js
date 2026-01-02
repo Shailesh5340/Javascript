397c0780fd00aa258d1b67a4c5be930bb7760df7
Shailesh Mandani <shaileshmandani5304@gmail.com>
2026-01-02 12:26:26 +0000

    data-types summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+//Array
+const heroes = ["Shaktiman", "nagraraj" ,"Doga"]
+
+
+let myObj = {
+    name: "Hitesh",
+    age:  20,
+}
+
+const myFunction = function(){
+    console.log("Hello World!!");
+    
+}
+console.log(typeof myFunction)
+
+let myYoutubename = "shaileshmandani"
+
+let anothername = myYoutubename
+
+console.log(anothername)
+console.log(myYoutubename)
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "shailesh@google.com"
+
+console.log( userOne.email);
+console.log( userTwo.email);
